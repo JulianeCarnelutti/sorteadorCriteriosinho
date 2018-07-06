10f9995b01dd51de8078de879c9f636d27421d01
LucasReinaldo <lucasreinaldo.demelo@hotmail.com>
2018-07-06 20:31:56 -0300

    base do projeto

---
+++ src/java/com/sorteadorcriteriosinho/model/Cadastro.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sorteadorcriteriosinho.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author LucasReinaldo
+ */
+
+public class Cadastro {
+	
+private static Cadastro instancia;
+    
+    public static Cadastro obterInstancia() {
+        if (instancia == null)
+            instancia = new Cadastro();
+        return instancia;
+    }
+    
+    EntityManager em = Persistence
+            .createEntityManagerFactory("UsuariosPU")
+            .createEntityManager();
+    
+    public void adicionarUsuario(Usuario u) {
+        em.getTransaction().begin();
+        em.persist(u);
+        em.getTransaction().commit();
+    }
+    
+    public Usuario obterUsuario(String email) {
+        return em.find(Usuario.class, email);
+    }
+    
+}
