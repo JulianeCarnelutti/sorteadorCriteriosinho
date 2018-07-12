747b373bffa9de1b2c7327e9a650ba25f0cf9192
gustavomac89 <gustavomac89@gmail.com>
2018-07-12 19:07:58 -0300

    Adicionado classes Pessoa e Turma

---
+++ src/java/com/sorteadorcriteriosinho/model/Turma.java
@@ -0,0 +1,35 @@
+package com.sorteadorcriteriosinho.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+public class Turma {
+    	
+	@id
+	private int id;
+	private String nome;
+	private ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+	
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public ArrayList<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setIdade(ArrayList<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+
+	
+
+}
